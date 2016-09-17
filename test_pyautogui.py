406e3b976358fbbfae3de3c1c7684eaec9ac6c0a
SACCFFT <reck98@gmail.com>
2016-09-17 12:31:49 -0400

    asdf

--- test_pyautogui.py
+++ test_pyautogui.py
@@ -1,56 +1,89 @@
 import pyautogui
 import time
 import os
+import subprocess
 
-def ourSleep(duration, checkFunction, args):
+
+
+def getBrightness():
+    cmd = ['brightness -l']
+    output = subprocess.Popen( cmd, shell = True, stdout=subprocess.PIPE).communicate()[0].decode("utf-8")
+    #print(output)
+    #print(getBrightnessLevel(output[0]))
+    return getBrightnessLevel(output)
+
+
+
+def getBrightnessLevel(s):
+    print(s)
+    return(s.strip().split(' ')[-1])
+    '''
+    newString=""
+    for index in (len(s)-11,len(s)-3):
+        newString+=str(s[index])
+    return newString
+    '''
+
+def setBrightness(s):
+    os.system('brightness '+s)
+
+
+globalBrightness = getBrightness()
+
+def ourSleep(duration, checkFunction):
     checkInterval = 0.1
     timerCount = 0
     while (timerCount < duration):
-        if (not checkFunction(*args)):
+        if (not checkFunction()):
             return -1
         time.sleep(checkInterval)
         timerCount += checkInterval
     return 0
 
-def smartIntervalExecute(longInterval, shortInterval, checkFunction, mainFunction, args = (), args2 = ()):
+def smartIntervalExecute(longInterval, shortInterval, checkFunction, mainFunction, args = ()):
     interval = longInterval
-    #print(args)
-    while (checkFunction(*args)):
-        mainFunction(*args2)
-        code = ourSleep(interval, checkFunction, args)
+    while (checkFunction()):
+        mainFunction(*args)
+        code = ourSleep(interval, checkFunction)
         if (code == -1):
             return
         interval = shortInterval
 
-def scroll(clicks,acceleration,checkFunction, arg = ()):
+def scroll(clicks,acceleration,checkFunction):
     if (acceleration==0):
-        smartIntervalExecute(0.40,0.40,checkFunction,pyautogui.scroll, args = arg, args2 = (clicks,))
+        smartIntervalExecute(0.40,0.40,checkFunction,pyautogui.scroll,(clicks,))
     else:
-        smartIntervalExecute(0.25,0.10,checkFunction,pyautogui.scroll,args = arg, args2 = (clicks+acceleration,))
+        smartIntervalExecute(0.25,0.10,checkFunction,pyautogui.scroll,(clicks+acceleration,))
 
-def leftTab(checkFunction, arg = ()):
+def leftTab(checkFunction):
     pyautogui.keyDown('command')
     pyautogui.press('tab')
     for i in range(0,2):
         pyautogui.press('left')
-    smartIntervalExecute(2,0.75, checkFunction, pyautogui.press,args = arg, args2 = ('left',))
+    smartIntervalExecute(2,0.75, checkFunction, pyautogui.press,('left',))
     pyautogui.keyUp('command')
 
-def rightTab(checkFunction, arg = ()):
+def rightTab(checkFunction):
     pyautogui.keyDown('command')
     pyautogui.press('tab')
-    smartIntervalExecute(2, 0.75, checkFunction, pyautogui.press, args = arg, args2 = ('right',))
+    smartIntervalExecute(2, 0.75, checkFunction, pyautogui.press, ('right',))
     pyautogui.keyUp('command')
 
 def screenshot():
     return pyautogui.screenshot()
 
 def screenOff():
-    os.system('brightness 1')
+    global globalBrightness
+    globalBrightness= getBrightness()
+    os.system('brightness [-v]')
+
+def screenOn():
+    setBrightness(globalBrightness)
 
 #input("Waiting")
 #os.system('say "hello world"')
-screenOff()
+#setBrightness(getBrightness())
+    #jalkdsjfkl
 
     
 #_thread.start_new_thread(test, ())
